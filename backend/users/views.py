3ca39622570a8e31af7b0d60b1cfa051975209d6
harshil-mistry <harshilmistry31@gmail.com>
2025-08-02 13:50:00 +0530

    Backend improved

--- backend/users/views.py
+++ backend/users/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
-
 # Create your views here.
+from rest_framework import generics
+from .serializers import UserSerializer
+from rest_framework.permissions import AllowAny
+
+class UserRegistrationView(generics.CreateAPIView):
+    serializer_class = UserSerializer
+    permission_classes = [AllowAny]
